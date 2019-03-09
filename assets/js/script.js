06b6334d12d72b605fce7bd54d76d55724edddd9
Marius Sarca <marius.sarca@gmail.com>
2019-03-09 18:11:01 +0200

    Refactored

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,81 @@
-(function($){
+function toggleShow(id) {
+    let target = document.getElementById(id);
+    let overlay = document.getElementById('drawer-overlay');
+    target.classList.toggle("show");
+    overlay.classList.toggle("show");
+    if (overlay.classList.contains('show')) {
+        overlay.onclick = function () {
+            toggleShow(id);
+        }
+    } else {
+        overlay.onclick = null;
+    }
+}
 
-    $(document).ready(function(){
-        var visible = false;
-        $(window).scroll(function(){
-            var top = $(this).scrollTop();
+function toggleDropdown(id) {
+    document.getElementById(id).classList.toggle("show");
+}
 
-            if (top >= 100 && !visible) {
-                visible = true;
-                $('#back-to-top').fadeIn().show();
-            }
-            if(top < 100 && visible){
-                visible = false;
-                $('#back-to-top').fadeOut();
-            }
+function scrollSpy(selector) {
+    let element = document.querySelector(selector);
+    if (!element) {
+        return;
+    }
+
+    let entries = [];
+    let links = element.querySelectorAll('a[href^="#"]');
+
+
+    for (let i = 0, l = links.length; i < l; i++) {
+        let currLink = links[i];
+        let refElement = document.querySelector(currLink.getAttribute('href'));
+        entries.push({
+            e: currLink,
+            r: refElement
         });
+    }
 
-        $('#back-to-top').click(function(){
-            $('html, body').animate({scrollTop : 0},800);
-            return false;
-        });
+    let entriesLength = entries.length;
+    let currentElement = null;
 
-    });
+    let callback = function (event) {
+        let scrollPos = window.scrollY;
+        let hasActive = false;
 
-    $('h2[id],h3[id],h4[id],h5[id],h6[id]').each(function () {
-        var child = $('<a href="#' + this.getAttribute('id') + '" class="ml-1 text-dark link-anchor"><span class="fas fa-hashtag"></span></a>');
-        this.appendChild(child.get(0));
-    });
+        for (let i = 0; i < entriesLength; i++) {
+            let entry = entries[i];
+            let currLink = entry.e;
+            let refElement = entry.r;
 
-    $('[data-toggle=tooltip]').tooltip();
-})(jQuery);
+            if (!refElement || !currLink) {
+                continue;
+            }
+
+            if (refElement.offsetTop <= scrollPos) {
+                if (currentElement !== currLink) {
+                    if (currentElement !== null) {
+                        currentElement.classList.remove('active');
+                    }
+                    currentElement = currLink;
+                    currentElement.classList.add('active');
+                }
+                hasActive = true;
+            }
+        }
+
+        if (!hasActive && currentElement !== null) {
+            currentElement.classList.remove('active');
+            currentElement = null;
+        }
+    };
+
+    document.addEventListener('scroll', callback);
+}
+
+window.addEventListener('load', function () {
+    scrollSpy('#on-this-page-section');
+
+    document.querySelectorAll('h2[id],h3[id],h4[id],h5[id],h6[id]').forEach(function (element) {
+        // append a hash tag
+    })
+});
